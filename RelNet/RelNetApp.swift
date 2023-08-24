1a3a1e05f83f5a9df8c7c5dfaed69a2a37f8f365
yyokii <yyokii.h@gmail.com>
2023-08-24 11:09:14 +0900

    Initial Commit

---
+++ RelNet/RelNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelNetApp.swift
+//  RelNet
+//
+//  Created by Higashihara Yoki on 2023/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RelNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
