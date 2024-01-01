291de256d932cb103bbdbfeaa27f1547023fcdcf
yyokii <yyokii.h@gmail.com>
2024-01-02 00:10:24 +0900

    🩹

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # GoogleService-Info.plistをXcode Cloudの環境変数より生成
-echo $GOOGLE_SERVICE_INFO > ../RelNet/GoogleService-Info.plist
+echo $GOOGLE_SERVICE_INFO > ../RelNet/App/GoogleService-Info.plist
